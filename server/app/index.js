eb1524b4f3b1b4decf89b5eaea58ca2e6ab6aaea
Kasador <hunterstevenshaw@gmail.com>
2025-06-04 17:26:47 -0700

    FEAT: Updated server backend structure

---
+++ server/app/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const routeHandler = require('./routes');
+
+const app = express();
+
+// middlewares 
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: `Server is up and running on port: ${PORT}`,
+    });
+});
+
+app.use('/api/v1', routeHandler);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,22 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
+const app = require("./app")
 require('dotenv').config();
 
-const app = express();
-
 const PORT = process.env.PORT || 3000;
-
-// middlewares 
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: `Server is up and running on port: ${PORT}`,
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port: ${PORT}`);
