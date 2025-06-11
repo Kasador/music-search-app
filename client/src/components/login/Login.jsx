a2915e1249f79b4809b12681959d8112380d8109
Kasador <hunterstevenshaw@gmail.com>
2025-06-11 13:38:07 -0700

    Issue: login route not redirecting user to login to Spotify.

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 // image for bg of login
+import axios from 'axios';
 import LoginBG from '../../assets/images/login_img.png'
 // import SCSS
 import './Login.scss'
@@ -7,6 +8,16 @@ import { FaSpotify } from "react-icons/fa";
 import { FaArrowRightToBracket } from "react-icons/fa6";
 
 function Login() {
+
+        const loginUser = async () => {
+            try {
+                const getdata = await axios.get('http://localhost:3000/api/v1/login')
+                console.log(getdata)
+            } catch (error) {
+                throw new Error(error)
+            }
+        };
+
     return (
         <section className="LoginPage">
             <img src={LoginBG} alt="Background image for the login page." />
@@ -14,7 +25,7 @@ function Login() {
                 <FaSpotify className="SpotifyIcon"/>
                 <h1>Please Login</h1>
                 <p>In order to seach for artists or songs, you must login to your Spotify account.</p>
-                <button>
+                <button onClick={loginUser}>
                     <span>Login <FaArrowRightToBracket /></span>  
                 </button>
             </div>
