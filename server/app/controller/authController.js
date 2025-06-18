643b874b88bd2dbef1fee54e857fddc12a863fec
Kasador <hunterstevenshaw@gmail.com>
2025-06-18 14:10:31 -0700

    Feat: Returns data to the frontend.

--- server/app/controller/authController.js
+++ server/app/controller/authController.js
@@ -1,13 +1,31 @@
 // auth spotify controller
+const axios = require('axios');
 
 const OAuth = async (req, res) => {
-    const { token, input } = req.headers;
+    const { token, input} = req.headers;
+    const { access_token } = req.user;
+
     console.log("Input Value:", input);
     console.log("Token Value:", token);
+    console.log("Access Value:", access_token);
 
-    res.status(200).json({
+    const data = await axios.get("https://api.spotify.com/v1/search", { // https://developer.spotify.com/documentation/web-api/reference/search
+        headers: { // headers to auth with access token... 
+            Authorization: `Bearer ${access_token}`
+        },
+        params: { // params to query
+            q: input,
+            type: 'track',
+            limit: 10
+        }
+    });
+
+    console.log(data.data.tracks);
+
+    return res.status(200).json({
         success: true,
-        message: `Valid tokens and input.`
+        message: `Valid tokens and input.`,
+        data: data.data.tracks
     })
 };
 
--- server/app/middleware/isAuth.js
+++ server/app/middleware/isAuth.js
@@ -1,8 +1,21 @@
 // auth middleware
+const jwt = require('jsonwebtoken');
+
 const isAuth = async (req, res, next) => {
     const { token } = req.headers;
-
     console.log('Middleware Token Value:', token);
+
+    const access = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Middleware Token Decoded:', access);
+    console.log('Middleware Access Token', access.access_token);
+    // const accessToken = access.access_token
+
+    // req.user = accessToken;
+    req.user = access;
+    // req.headers.accessToken;
+    // next((req) = () => {
+    //     req.headers = accessToken;
+    // });
     next();
 }
 
