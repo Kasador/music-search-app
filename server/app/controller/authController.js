bab20bc46784dcb4babc771fb555e6880ae3ad4f
Kasador <hunterstevenshaw@gmail.com>
2025-06-18 12:02:25 -0700

    Feat: Sent the token and input value from search bar to the backend using headers.

--- server/app/controller/authController.js
+++ server/app/controller/authController.js
@@ -1,8 +1,13 @@
 // auth spotify controller
 
 const OAuth = async (req, res) => {
+    const { token, input } = req.headers;
+    console.log("Input Value:", input);
+    console.log("Token Value:", token);
+
     res.status(200).json({
-        message: `My name is ${req.user}`
+        message: `My name is ${req.user}.`,
+        token: `${token}`
     })
 };
 
